250d498fd37222250bb08933cb56e968861dea23
Alexandru Schneider <schneider.alex@icloud.com>
2025-12-02 15:20:53 +0100

    *Neue Eintrag

--- taschenrechner.py
+++ taschenrechner.py
@@ -12,4 +12,6 @@ elif operator == "*":
     ergebnis = zahl_1 * zahl_2
     print(f"Das Ergebnis von {zahl_1} * {zahl_2} ist {ergebnis}")
 
-
+elif operator == "%":
+    ergebnis = zahl_1 % zahl_2
+    print(f"Das Ergebnis von {zahl_1} % {zahl_2} ist {ergebnis}")
