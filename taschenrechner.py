e3ec55b0b631a16d77bc8633ec42170a692f9a55
Alexandru Schneider <schneider.alex@icloud.com>
2025-12-02 11:50:17 +0100

    6.Eintrag

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,1 +1,2 @@
 print("Hallo! Willkommen zum einfachen Taschenrechner.")
+print("Du kannst zwei Zahlen eingeben und eine Operation auswählen: Addition (+), Subtraktion (-), Multiplikation (*), Division (/).")
