f40229467218093af24d0ba04f5e639ec2a557fd
Alexandru Schneider <schneider.alex@icloud.com>
2025-12-02 15:04:37 +0100

    inputs & + operato

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,1 +1,8 @@
-print("Test Alex")
+zahl_1 = int(input("Gib die erste Zahl ein: "))
+operator = input("Gib den Operator ein (+, -, *, /): ")
+zahl_2 = int(input("Gib die zweite Zahl ein: "))
+
+if operator == "+":
+    ergebnis = zahl_1 + zahl_2
+    print(f"Das Ergebnis von {zahl_1} + {zahl_2} ist {ergebnis}")
+    
